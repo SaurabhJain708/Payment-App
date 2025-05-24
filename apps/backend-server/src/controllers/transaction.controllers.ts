07fc35629bd36ac0464d7dc0795c7b9013c1182b
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-05-25 03:31:35 +0530

    Added send money controller

--- apps/backend-server/src/controllers/transaction.controllers.ts
+++ apps/backend-server/src/controllers/transaction.controllers.ts
@@ -28,3 +28,79 @@ export const GetWalletBalanceController = async (
     });
   }
 };
+
+export const SendMoneyController = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { amount, recipientId } = req.body;
+    const userId = req.sessionData?.userId;
+    if (!amount || !recipientId || typeof amount !== "number" || amount <= 0) {
+      res.status(400).json({
+        success: false,
+        message: "Amount and recipient ID are required",
+      });
+      return;
+    }
+    const sender = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    const recipient = await prisma.user.findUnique({
+      where: { id: recipientId },
+    });
+    if (!sender || !recipient || sender.id === recipient.id) {
+      res.status(404).json({
+        success: false,
+        message: "Sender or recipient not found",
+      });
+      return;
+    }
+    if (sender.amount < amount * 100) {
+      res.status(400).json({
+        success: false,
+        message: "Insufficient balance",
+      });
+      return;
+    }
+    // Transaction Logic
+    await prisma.$transaction(async (prisma) => {
+      const deductMoneyFromSender = await prisma.user.update({
+        where: { id: userId },
+        data: { amount: { decrement: amount * 100 } },
+      });
+      if (!deductMoneyFromSender) {
+        throw new Error("Failed to deduct money from sender");
+      }
+      const addMoneyToRecipient = await prisma.user.update({
+        where: { id: recipientId },
+        data: { amount: { increment: amount * 100 } },
+      });
+      if (!addMoneyToRecipient) {
+        throw new Error("Failed to add money to recipient");
+      }
+
+      // Create transaction record
+      const transactionRecord = await prisma.transaction.create({
+        data: {
+          senderId: userId as string,
+          receiverId: recipientId,
+          amount: amount * 100,
+        },
+      });
+      if (!transactionRecord) {
+        throw new Error("Failed to create transaction record");
+      }
+    });
+    res.status(200).json({
+      success: true,
+      message: "Money sent successfully",
+    });
+  } catch (error) {
+    console.error("Error sending money:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to send money",
+    });
+  }
+};
