1e4b857aa5d98abd6e9a1fce18a53767c83c0935
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-05-25 03:40:25 +0530

    Added get transaction history controller

--- apps/backend-server/src/controllers/transaction.controllers.ts
+++ apps/backend-server/src/controllers/transaction.controllers.ts
@@ -104,3 +104,36 @@ export const SendMoneyController = async (
     });
   }
 };
+
+export const GetTransactionHistoryController = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const userId = req.sessionData?.userId;
+    const page = parseInt(req.query.page as string) || 0;
+    const transactions = await prisma.transaction.findMany({
+      where: {
+        OR: [{ senderId: userId }, { receiverId: userId }],
+      },
+      orderBy: { createdAt: "desc" },
+      skip: page * 10,
+      take: 10,
+    });
+    res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          transactions,
+          "Transaction history fetched successfully"
+        )
+      );
+  } catch (error) {
+    console.error("Error fetching transaction history:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch transaction history",
+    });
+  }
+};
